23a0d1f243fe25b0e126672d127f67c8846ab2a1
saddlepiggy <55365185+saddlepiggy@users.noreply.github.com>
2020-02-13 17:34:00 -0600

    Added ColorSensorTest (changes color of RC screen to color that the sensor detects), added Stone Arm servo to a couple of autonomous programs.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -1,69 +1,121 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.Locale;
+
 /**
- * Created by George on 9/27/2019.
+ * Created by George on 2/7/2020.
  */
-@Autonomous(name = "Blue Foundation Park Middle", group = "Competition")
 
-public class BlueFoundParkMiddle extends LinearOpMode {
+@Autonomous(name = "Color Sensor Test", group = "Testing")
 
-    MecanumReference ref = new MecanumReference();
+public class ColorSensorTest extends LinearOpMode {
 
-    DcMotor fl;
     DcMotor fr;
-    DcMotor bl;
+    DcMotor fl;
     DcMotor br;
+    DcMotor bl;
+    DcMotor arm;
+    CRServo wrist;
+    Servo clamp;
     Servo foundr;
     Servo foundl;
     Servo stone;
+    ColorSensor colorSensor;
 
     @Override
     public void runOpMode() {
 
-        fl = hardwareMap.dcMotor.get("front left");
         fr = hardwareMap.dcMotor.get("front right");
-        bl = hardwareMap.dcMotor.get("back left");
+        fl = hardwareMap.dcMotor.get("front left");
         br = hardwareMap.dcMotor.get("back right");
+        bl = hardwareMap.dcMotor.get("back left");
+        arm = hardwareMap.dcMotor.get("arm");
+        wrist = hardwareMap.crservo.get("wrist");
+        clamp = hardwareMap.servo.get("left clamp");
         foundr = hardwareMap.servo.get("foundation right");
         foundl = hardwareMap.servo.get("foundation left");
         stone = hardwareMap.servo.get("stone arm");
+        colorSensor = hardwareMap.colorSensor.get("color sensor");
 
-        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         fr.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         br.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         fl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         bl.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F, 0F, 0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // sometimes it helps to multiply the raw RGB values with a scale factor
+        // to amplify/attentuate the measured values.
+        final double SCALE_FACTOR = 255;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+        // wait for the start button to be pressed.
         waitForStart();
 
-        //foundation = 34.5 by 18.5
-        driveXY(15, 1, "backward");
-        driveXY(12, 1, "right");
-        driveXY (15, 1, "backward");
-        driveXY (7, .25, "backward");
-        foundr.setPosition(0);
-        foundl.setPosition(1);
-        sleep(2000);
-        driveXY (52, 1, "forward");
-        sleep(100);
-        foundr.setPosition(.5);
-        foundl.setPosition(0.5);
-        sleep(2000);
-        driveXY(6, 1, "forward");
-        driveXY(24, 1, "left");
-        driveXY(30, 1, "backward");
-        driveXY (15, 1, "left");
-        stone.setPosition(1);
+        // loop and read the RGB and distance data.
+        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
+            while (opModeIsActive()) {
+            // convert the RGB values to HSV values.
+            // multiply by the SCALE_FACTOR.
+            // then cast it back to int (SCALE_FACTOR is a double)
+            Color.RGBToHSV((int) (colorSensor.red() * SCALE_FACTOR),
+                    (int) (colorSensor.green() * SCALE_FACTOR),
+                    (int) (colorSensor.blue() * SCALE_FACTOR),
+                    hsvValues);
+
+            // send the info back to driver station using telemetry function.
+
+            telemetry.addData("Alpha", colorSensor.alpha());
+            telemetry.addData("Red  ", colorSensor.red());
+            telemetry.addData("Green", colorSensor.green());
+            telemetry.addData("Blue ", colorSensor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+
+            // change the background color to match the color detected by the RGB sensor.
+            // pass a reference to the hue, saturation, and value array as an argument
+            // to the HSVToColor method.
+            relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                }
+            });
+
+            telemetry.update();
+        }
+
+        // Set the panel back to the default color
+        relativeLayout.post(new Runnable() {
+            public void run() {
+                relativeLayout.setBackgroundColor(Color.WHITE);
+            }
+        });
     }
+
 
     public void driveXY(float inches, double speed, String direction) {
 
@@ -74,8 +126,8 @@ public class BlueFoundParkMiddle extends LinearOpMode {
         //8 inches per rotation
         //140 counts per Y inch
 
-        float XcountsPerInch = ref.XcountsPerInch;
-        float YcountsPerInch = ref.YcountsPerInch;
+        float XcountsPerInch = 86f;
+        float YcountsPerInch = 140f;
 
         fr.setMode(DcMotor.RunMode.RESET_ENCODERS);
         br.setMode(DcMotor.RunMode.RESET_ENCODERS);
@@ -94,17 +146,17 @@ public class BlueFoundParkMiddle extends LinearOpMode {
             fl.setTargetPosition(Math.round(inches * XcountsPerInch));
             bl.setTargetPosition(Math.round(inches * XcountsPerInch));
         }
-        if (direction == "right") {
-            fr.setTargetPosition(-Math.round(inches * YcountsPerInch));
-            br.setTargetPosition(Math.round(inches * YcountsPerInch));
-            fl.setTargetPosition(-Math.round(inches * YcountsPerInch));
-            bl.setTargetPosition(Math.round(inches * YcountsPerInch));
-        }
         if (direction == "left") {
             fr.setTargetPosition(Math.round(inches * YcountsPerInch));
             br.setTargetPosition(-Math.round(inches * YcountsPerInch));
             fl.setTargetPosition(Math.round(inches * YcountsPerInch));
             bl.setTargetPosition(-Math.round(inches * YcountsPerInch));
+        }
+        if (direction == "right") {
+            fr.setTargetPosition(-Math.round(inches * YcountsPerInch));
+            br.setTargetPosition(Math.round(inches * YcountsPerInch));
+            fl.setTargetPosition(-Math.round(inches * YcountsPerInch));
+            bl.setTargetPosition(Math.round(inches * YcountsPerInch));
         }
 
         fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -138,7 +190,7 @@ public class BlueFoundParkMiddle extends LinearOpMode {
         //60 degrees per rotation
         //18.6 countsPerDegree counts per degree
 
-        float countsPerDegree = ref.countsPerDegree;
+        float countsPerDegree = 18.666f;
 
         fr.setMode(DcMotor.RunMode.RESET_ENCODERS);
         br.setMode(DcMotor.RunMode.RESET_ENCODERS);
@@ -182,45 +234,33 @@ public class BlueFoundParkMiddle extends LinearOpMode {
         bl.setMode(DcMotor.RunMode.RESET_ENCODERS);
     }
 
-/*    public void lift (float inches, double speed, String direction) {
+    public void arm(int locks, double speed, String direction) {
+
         //1120 counts per rotation
-        //3 1/4 inches per rotation
-        //344.6 counts per inch
+        // degrees per rotation
+        // countsPerLock counts per degree
 
-        float lCountsPerInch = 140;
+        float countsPerLock = 1260;
 
-        if (direction == "lift") {
-            lift.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        arm.setMode(DcMotor.RunMode.RESET_ENCODERS);
 
-            lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            lift.setTargetPosition(Math.round(299*inches));
-
-            lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            lift.setPower(speed);
-
-            while (lift.isBusy()) {
-
-            }
-            lift.setPower(0);
+        if(direction == "down") {
+            arm.setTargetPosition(Math.round(locks * countsPerLock));
+        }
+        if(direction == "up") {
+            arm.setTargetPosition(-Math.round(locks * countsPerLock));
         }
 
-        if (direction == "drop") {
-            lift.setMode(DcMotor.RunMode.RESET_ENCODERS);
+        arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        arm.setPower(speed);
 
-            lift.setTargetPosition(-Math.round(299*inches));
+        while (arm.isBusy()) {
 
-            lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-            lift.setPower(speed);
-
-            while (lift.isBusy()) {
-
-            }
-            lift.setPower(0);
         }
-    } */
+        arm.setPower(0);
+
+        arm.setMode(DcMotor.RunMode.RESET_ENCODERS);
+    }
+
 }
